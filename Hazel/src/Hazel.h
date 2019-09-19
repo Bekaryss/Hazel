966633016a970e66cdabe56eedb6fd3016b893c5
bekaryss <bekarys.kuralbai@gmail.com>
2019-09-20 02:42:20 +0600

    Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hezel/Application.h"
+
+// ---Entry Point------------------
+#include "Hezel/EntryPoint.h"
+// --------------------------------
