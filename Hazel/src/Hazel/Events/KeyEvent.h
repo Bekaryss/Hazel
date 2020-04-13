5a013d879e6c6e265479cba8f7dbef35039ccdd7
Bekaryss <bekarys.kuralbay@digitech7.by>
2020-04-13 18:55:11 +0300

    Window Events

--- Hazel/src/Hazel/Events/KeyEvent.h
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -36,4 +36,19 @@ namespace Hazel {
 		int m_RepeatCount;
 	};
 
+	class HAZEL_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
 }
